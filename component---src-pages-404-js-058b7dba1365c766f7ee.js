b5d6f689b64571575c5bda0390292d340a79f935
Vinh <nguyenvinh1997@yahoo.de>
2022-04-02 19:09:41 +0200

    Updates

---
+++ component---src-pages-404-js-058b7dba1365c766f7ee.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,t,n){"use strict";n.r(t);var l=n("FdF9"),a=n("Bl7J"),u=n("Wbzz"),r=n("vrFN");t.default=function(){return l.default.createElement(a.a,null,l.default.createElement(r.a,{title:"404: Not found"}),l.default.createElement("strong",null,"404: Not Found"),l.default.createElement("p",null,"Die Seite die Sie suchen ist leider nicht verfügbar."),l.default.createElement(u.Link,{to:"/oal-website-de"},"Zurück zum Startseite"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-058b7dba1365c766f7ee.js.map
